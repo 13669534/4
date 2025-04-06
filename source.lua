cc15c8c3889b06c252a7e66327079bcbb0509e87
13669534 <riskingfreedom@gmail.com>
2025-04-06 21:21:12 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,14 +1,3 @@
 
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/13669534/4/main/lib.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/13669534/4/main/lib.lua"))()
 
-local Window = Library:CreateWindow("HVH", Vector2.new(492, 598), Enum.KeyCode.RightControl)
-
-local Tabs = {
-    Main = Window:CreateTab("Main"),
-    Combat = Window:CreateTab("Combat"),
-    Visuals = Window:CreateTab("Visuals"),
-    Movement = Window:CreateTab("Movement"),
-    Settings = Window:CreateTab("Settings")
-}
-
--- Tabs exist only for navigation. No buttons or UI elements added inside.
